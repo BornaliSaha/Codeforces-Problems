1e30c8116d43f355f81fb78829f516fb1ddcb912
Bornali Saha <68811059+BornaliSaha@users.noreply.github.com>
2021-09-04 14:34:31 +0600

    Between the Offices

---
+++ 867A.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+   int n,i=0,j=0,len=0,ct2=0;
+   char arr[100];
+   cin>>n>>arr;
+   if(arr[0]=='S'&&arr[n-1]=='F')
+   {
+       cout<<"YES";
+   }
+   else cout<<"NO";
+ 
+    return 0;
+}
