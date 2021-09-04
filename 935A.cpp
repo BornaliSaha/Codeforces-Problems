73800d162727d25045e0bfbda726db7049e5037e
Bornali Saha <68811059+BornaliSaha@users.noreply.github.com>
2021-09-04 14:38:38 +0600

    Fafa and his Company

---
+++ 935A.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    int n,ck=1;
+    cin>>n;
+    for(int i=2;i<=n/2;i++)
+    {
+        if(n%i==0)
+        {
+            ck++;
+        }
+    }
+    cout<<ck;
+ 
+ 
+    return 0;
+}
