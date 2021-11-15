8eb090ee83689671ad1e33e6370efdf9c0b87d56
Bornali Saha <bornalisaha.duet@gmail.com>
2021-11-15 20:03:35 +0600

    Mathematical Addition

---
+++ 1584A.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        ll u,v;
+        cin>>u>>v;
+        ll x= -(u*u);
+        ll y= v*v;
+        cout<<x<<" "<<y<<endl;
+
+    }
+}
