f97e39a08cc0ed097c20fc854cc2b95fdf518608
Bornali Saha <bornalisaha.duet@gmail.com>
2021-12-18 12:33:04 +0600

    Find Array

---
+++ 1608A.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+ 
+void saha()
+{
+    ll n;
+    cin>>n;
+    for(int i=2;i<=n+1;i++)
+        cout<<i<<" ";
+    cout<<endl;
+ 
+}
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+        saha();
+}
